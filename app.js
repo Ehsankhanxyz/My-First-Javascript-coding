be85fc6cebcd58440516f36a010b7205c7ffe520
Muhammad Ahsan <ehsankhanxyz@gmail.com>
2017-03-14 01:01:51 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,13 @@
+alert("Hello World");
+// var a= "10";
+// var b = 20;
+// var c = a + b;
+
+// alert(c+ a);
+
+var  abc=11;
+var abcd = 20;
+
+var sum= abc + abcd+ abc;
+sum= sum + abc;
+alert (sum);
